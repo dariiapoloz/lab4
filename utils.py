3693cf258412c98e38c32171c34eb1e1b9092fd9
darphri <dariiapoloz@gmail.com>
2026-03-01 19:02:13 +0200

    Add gcd function

--- utils.py
+++ utils.py
@@ -4,4 +4,9 @@ def factorial(n):
 	if n == 0 or n == 1:
         	return 1
 	res = n * factorial(n - 1)
-return res
+		return res
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
