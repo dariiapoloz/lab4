e71f5daa04c9dd19cd538070b38524de0b38ca31
Dariia Poloz <dariiapoloz@knu.ua>
2026-03-01 19:09:39 +0200

    add func is_power_of_five

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from utils import factorial, gcd
+from utils import factorial, gcd, is_power_of_five
 a=int(input())
 b=int(input())
 n=int(input())
 print(factorial(n))
 print(gcd(a,b))
+print(is_power_of_five(n))
